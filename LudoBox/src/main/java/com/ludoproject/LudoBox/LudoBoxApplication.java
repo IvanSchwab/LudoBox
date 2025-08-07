ba99e61c12f75afb97c44d6a23f8bfe742c120d1
Ivan <ivan_schwab@outlook.com>
2025-08-07 15:41:37 -0300

    commit

---
+++ LudoBox/src/main/java/com/ludoproject/LudoBox/LudoBoxApplication.java
@@ -0,0 +1,13 @@
+package com.ludoproject.LudoBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudoBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudoBoxApplication.class, args);
+	}
+
+}
---
+++ LudoBox/src/test/java/com/ludoproject/LudoBox/LudoBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludoproject.LudoBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudoBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
